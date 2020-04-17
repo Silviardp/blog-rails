07c32b0af6db4410452a1f407e4c68f2daec66c4
Silvia Rocha <silviadprocha@gmail.com>
2020-04-17 15:58:22 +0900

    add routes and posts controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get "/posts/new", to: 'pages#home'
+  get "/posts/:id", to: 'pages#home'
+
+  #API
+  namespace :api, defaults: { format: :json} do
+    namespace :v1 do
+      resources :posts, only: [ :index, :show, :create ]
+    end
+  end
+
 end
